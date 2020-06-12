ba04b3c08e43ae8b8c1561e7b78f6dab848db924
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2020-06-12 17:01:57 -0400

    Delete add_Codon_Usage_data_into_Sodapop_cell_file_from_Kasuza_db_like_file.sh

--- add_Codon_Usage_data_into_Sodapop_cell_file_from_Kasuza_db_like_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this script takes in input the path of a file in Kazusa database codon usage-like format (1st input argument), convert it and add the data in the input .cell file (2nd input argument). It also remove all stop codons data, which are useless for CAI calculations.
-
-sed -e 's/([^()]*)/)/g' $1 | sed 's/)  /\n/g' | sed 's/)//g' | sed '/^$/d' | sed '/UAG/d' | sed '/UGA/d' | sed '/UAA/d' >> $2
-
-echo "##################################################################"
-echo "DON'T FORGET TO EXECUTE Sodasumm after Codon usage configurations are done for every species you want to simulate!!!"
-echo "##################################################################"
