7f33045fe5bf64344113027e9ad7dbdbe0fcd740
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2020-07-04 13:05:01 -0400

    Update add_Codon_Usage_data_into_Sodapop_cell_file_from_Kasuza_db_like_file.sh

--- tools/add_Codon_Usage_data_into_Sodapop_cell_file_from_Kasuza_db_like_file.sh
+++ tools/add_Codon_Usage_data_into_Sodapop_cell_file_from_Kasuza_db_like_file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#this script takes in input the path of a file in Kazusa database codon usage-like format (1st input argument), convert it and add the data in the input .cell file (2nd input argument). It also remove all stop codons data, which are useless for CAI calculations.
+#this script takes in input the path of a file in Kazusa database codon usage-like format (1st input argument) and adds the data in the input .cell file (2nd input argument). It also remove all stop codons data, which are useless for CAI calculations.
 
 sed -e 's/([^()]*)/)/g' $1 | sed 's/)  /\n/g' | sed 's/)//g' | sed '/^$/d' | sed '/UAG/d' | sed '/UGA/d' | sed '/UAA/d' >> $2
 
